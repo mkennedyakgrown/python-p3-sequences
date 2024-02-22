ed074ef9851c0201d8cc663313cce4a276e71c2f
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-02-22 10:42:50 -0600

    Completed Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib = [0, 1]
+        for i in range(1, length - 1):
+            fib.append(fib[i] + fib[i - 1])
+        print(fib)
